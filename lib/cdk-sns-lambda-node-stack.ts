d0c50d4f817e066ed9be19c717aaecd9711fafef
miguelcss <miguelcorreiasousa@gmail.com>
2022-09-29 18:40:56 +0100

    initial commit: cdk init app --language=typescript

---
+++ lib/cdk-sns-lambda-node-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkSnsLambdaNodeStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkSnsLambdaNodeQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
